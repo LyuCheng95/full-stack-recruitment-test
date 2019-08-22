fe83368a4168cf7b4cc1b114c732df0312ab1c47
jiaxiang227 <30530354+jiaxiang227@users.noreply.github.com>
2019-08-22 22:36:37 +0800

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import BpkText from 'bpk-component-text';
+import BpkPanel from 'bpk-component-panel';
+import flights from '../../flights.json';
+
 
 import STYLES from './App.scss';
 import Header from './../Header';
 
 const getClassName = className => STYLES[className] || 'UNKNOWN';
-
 const App = () => (
   <div className={getClassName('App')}>
     <Header />
     <main className={getClassName('App__main')}>
-      <BpkText tagName="p">Over to you...</BpkText>
       {/* TODO: Add an information blue header component here */}
-
       {/* TODO: Add a component to display results here */}
+      <div>
+        {flights.itineraries.map((i)=>{
+          return <BpkPanel>
+            {i.price + " " + flights.legs.filter((l)=>{return l.id === i.legs[0]})[0].departure_airport}
+          </BpkPanel>
+        })}
+      </div>
     </main>
   </div>
 );
